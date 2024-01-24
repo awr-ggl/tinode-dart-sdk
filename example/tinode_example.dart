993bcd886773368b780e95194525e5a6f680115a
Aries1711 <levfreey17@gmail.com>
2024-01-24 15:08:54 +0700

    renew websocket library version

--- example/tinode_example.dart
+++ example/tinode_example.dart
@@ -2,31 +2,41 @@ import 'package:tinode/tinode.dart';
 
 void main(List<String> args) async {
   var key = 'AQEAAAABAAD_rAp4DJh05a1HAwFT3A6K';
-  var host = 'sandbox.tinode.co';
+  var host = 'chat-demo.ggl.life';
 
   var loggerEnabled = true;
-  var tinode = Tinode('Moein', ConnectionOptions(host, key, secure: true), loggerEnabled);
+  var tinode = Tinode(
+      'Moein', ConnectionOptions(host, key, secure: true), loggerEnabled);
   await tinode.connect();
   print('Is Connected:' + tinode.isConnected.toString());
-  var result = await tinode.loginBasic('alice', 'alice123', null);
+  var result = await tinode.loginBasic('ariesfreey', '12345678', null);
   print('User Id: ' + result.params['user'].toString());
 
   var me = tinode.getMeTopic();
   me.onSubsUpdated.listen((value) {
     for (var item in value) {
-      print('Subscription[' + item.topic.toString() + ']: ' + item.public['fn'] + ' - Unread Messages:' + item.unread.toString());
+      print('Subscription[' +
+          item.topic.toString() +
+          ']: ' +
+          ' - Unread Messages:' +
+          item.unread.toString());
     }
   });
   await me.subscribe(MetaGetBuilder(me).withLaterSub(null).build(), null);
 
-  var grp = tinode.getTopic('grpWAFkncfrJtc');
+  var grp = tinode.getTopic('grpeTeKuPsh0Vw');
   grp.onData.listen((value) {
     if (value != null) {
       print('DataMessage: ' + value.content);
     }
   });
 
-  await grp.subscribe(MetaGetBuilder(tinode.getTopic('grpWAFkncfrJtc')).withLaterSub(null).withLaterData(null).build(), null);
-  var msg = grp.createMessage('This is cool', true);
+  await grp.subscribe(
+      MetaGetBuilder(tinode.getTopic('grpeTeKuPsh0Vw'))
+          .withLaterSub(null)
+          .withLaterData(null)
+          .build(),
+      null);
+  var msg = grp.createMessage('This is cool', false);
   await grp.publishMessage(msg);
 }
